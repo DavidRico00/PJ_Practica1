ffae2f2c6ce52c6684fc41df954c88c446341460
David Rico <davidriico00@gmail.com>
2025-04-09 12:47:08 +0200

    commit

--- Practica1/Assets/Scripts/PlayerScript.cs
+++ Practica1/Assets/Scripts/PlayerScript.cs
@@ -3,54 +3,93 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
 
-    public float Speed;
-    private float hor, ver, lessSpeed=0.15f;
+    public float Speed, attackRange;
+    private float horizontal, vertical, lessSpeed=0.15f;
 
     private Rigidbody2D rb2D;
     private Animator animator;
+    private bool atacando = false;
+    public LayerMask enemyLayer;
     
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        /*h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");*/
-        
-        if(Input.GetKey(KeyCode.A)) hor = -1.0f;
-        else if(Input.GetKey(KeyCode.D)) hor = 1.0f;
-        else hor = 0.0f;
+        if      (Input.GetKey(KeyCode.A))   horizontal = -1.0f;
+        else if (Input.GetKey(KeyCode.D))   horizontal = 1.0f;
+        else                                horizontal = 0.0f;
 
-        if(Input.GetKey(KeyCode.W)) ver = 1.0f;
-        else if(Input.GetKey(KeyCode.S)) ver = -1.0f;
-        else ver = 0.0f;
+        if      (Input.GetKey(KeyCode.W))   vertical = 1.0f;
+        else if (Input.GetKey(KeyCode.S))   vertical = -1.0f;
+        else                                vertical = 0.0f;
 
-        if(hor < 0.0f) transform.localScale = new Vector3(-0.4f, 0.4f, 1);
-        else if (hor > 0.0f) transform.localScale = new Vector3(0.4f, 0.4f, 1);
+        SetMovementAnimation();
 
-        if(ver > 0)
-            animator.SetInteger("walking", 2);
-        else if(ver < 0)
-            animator.SetInteger("walking", -1);
-        else if(hor != 0)
-            animator.SetInteger("walking", 1);
-        else
-            animator.SetInteger("walking", 0);
+        if      (Input.GetKey(KeyCode.UpArrow) && !atacando)        Attack(12);
+        else if (Input.GetKey(KeyCode.DownArrow) && !atacando)      Attack(6);
+        else if (Input.GetKey(KeyCode.LeftArrow) && !atacando)      Attack(9);
+        else if (Input.GetKey(KeyCode.RightArrow) && !atacando)     Attack(3);
+    }
 
+    private void SetMovementAnimation()
+    {
+        if      (vertical > 0)     animator.SetInteger("walking", 12);
+        else if (vertical < 0)     animator.SetInteger("walking", 6);
+        else if (horizontal > 0)   animator.SetInteger("walking", 3);
+        else if (horizontal < 0)   animator.SetInteger("walking", 9);
+        else                       animator.SetInteger("walking", 0);
     }
 
     void FixedUpdate()
     {
-        if(Mathf.Abs(hor) == 1.0f && Mathf.Abs(ver) == 1.0f)
-            rb2D.linearVelocity = new Vector2(hor * (Speed-lessSpeed), ver * (Speed-lessSpeed));
+        if(Mathf.Abs(horizontal) == 1.0f && Mathf.Abs(vertical) == 1.0f)
+            rb2D.linearVelocity = new Vector2(horizontal * (Speed-lessSpeed), vertical * (Speed-lessSpeed));
         else
-            rb2D.linearVelocity = new Vector2(hor * Speed, ver * Speed);
+            rb2D.linearVelocity = new Vector2(horizontal * Speed, vertical * Speed);
+    }
 
+    void Attack(int direccion)
+    {
+        Debug.Log("Atacando en direccion: " + direccion);
+        atacando = true;
+        animator.SetInteger("attack", direccion);
+        LanzarRaycast(direccion);
+    }
+
+    void NoAttack()
+    {
+        atacando = false;
+        animator.SetInteger("attack", 0);
+        animator.SetInteger("walking", 0);
+    }
+
+    void LanzarRaycast(int direccion)
+    {
+        Vector2 direction = Vector2.zero;
+
+        switch (direccion)
+        {
+            case 12: direction = Vector2.up; break;
+            case 6: direction =  Vector2.down; break;
+            case 3: direction =  Vector2.right; break;
+            case 9: direction =  Vector2.left; break;
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, attackRange, enemyLayer);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Enemigo golpeado: " + hit.collider.name);
+        }
+        else
+        {
+            Debug.Log("No hay enemigo en el rango de ataque.");
+        }
+
+        
     }
 }
